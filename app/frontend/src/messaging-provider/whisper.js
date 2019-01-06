94a47984262596a8427261291e5f410546f51742
Mathew Cormier <mathew.corm@gmail.com>
2019-01-06 17:49:34 -0500

    Add signMessage

--- app/frontend/src/messaging-provider/whisper.js
+++ app/frontend/src/messaging-provider/whisper.js
@@ -1,4 +1,6 @@
 const Web3 = window.Web3
+const web3 = window.web3
+const TTL = 60 * 60 * 24 // TTL of messages in seconds: 1 day 
 
 export class WhisperProvider {
 
@@ -9,29 +11,55 @@ export class WhisperProvider {
     }
 
     async _initialize() {
-        console.log('host: ', this._opts.host)
-        this._web3 = new Web3(new Web3.providers.HttpProvider(this._opts.host))
-        await this._web3.personal.unlockAccount(this._web3.eth.accounts[0], this._opts.accountPassword)
+        this._shhWeb3 = new Web3(new Web3.providers.HttpProvider(this._opts.host))
+        this._web3 = new Web3(web3.currentProvider)
+        await this._shhWeb3.personal.unlockAccount(this._shhWeb3.eth.accounts[0], this._opts.accountPassword)
     }
 
 
-    async post(message, topic) {
-        this._web3.shh.post({
+    async signMessage(message) {
+        return new Promise((res, rej) => {
+            this._web3.personal.sign(message, 
+                this._web3.eth.accounts[0], 
+                (err, result) => { 
+                    if (err)
+                        return rej(err)
+                    
+                    return res(result)
+                }
+            )
+        })
+    }
+
+
+    async post(message, signature, topic = '0x00000000') {
+        this._shhWeb3.shh.post({
             pubKey: this._opts.pubKey,
-            ttl: 7,
-            topic: '0x07678231',
+            ttl: TTL,
+            topic,
             powTarget: 2.01,
             powTime: 2,
-            payload: this._web3.fromUtf8(message)
+            payload: this._shhWeb3.fromUtf8(message)
         })  
     }
 
     subscribe(topic, cb) {
-        this._web3.shh.newMessageFilter({
+        this._shhWeb3.shh.newMessageFilter({
             privateKeyID: this._opts.privKey
-        }, (err, res) => cb({
-            ...res,
-            message: this._web3.toUtf8(res.payload)
-        }))
+        }, (err, res) => { 
+
+
+            cb({
+                ...res,
+                message: this._shhWeb3.toUtf8(res.payload)
+            })
+        }
+        
+        )
     }
+
+
+    
 }
+
+
